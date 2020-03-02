d38981d3b9ae9999bcdf0413f9c98b3fdce3d035
WebInfUa <web.inf.ua@gmail.com>
2020-03-03 01:35:51 +0200

    start version 0.1.0

--- src/redux/constants/LOGIN.ts
+++ src/redux/constants/LOGIN.ts
@@ -1,4 +1,5 @@
 export const LOGIN = "LOGIN";
+export const AUTO_LOGIN = "AUTO_LOGIN";
 export const LOGOUT = 'LOGOUT';
 export const LOGIN_ERROR = "AUTHENTICATION_ERROR";
 export const LOAD = 'LOAD';
